270319a1de7f9ab320cd0d5d725b1b4a2bd1f85a
Ruan Esterhuyse <ruan.esterhuyse@gmail.com>
2021-12-20 16:49:50 +0200

    update

--- game.js
+++ game.js
@@ -1,4 +1,13 @@
+var gamePattern =[];
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+
+
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4); 
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
 
 };
+
+console.log(randomNumber);
