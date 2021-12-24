ad804eed66f4b9a96f2d2aec4fc9d697c169c8a3
Ruan Esterhuyse <ruan.esterhuyse@gmail.com>
2021-12-24 09:49:21 +0200

    checkanswer

--- game.js
+++ game.js
@@ -20,7 +20,29 @@ $(".btn").click(function() {
   userClickedPattern.push(userChosenColour);
   playSound(userChosenColour);
   animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length-1);
 });
+
+function checkAnswer(currentLevel) {
+
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+    console.log("success");
+
+    if (userClickedPattern.length === gamePattern.length){
+
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+
+    }
+  } else {
+
+    console.log("wrong");
+
+  }
+
+}
 
 
 function nextSequence() {
